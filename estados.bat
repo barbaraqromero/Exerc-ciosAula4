c735715a2b4675b2821e35aadf1fd8d883f65b82
Bárbara Romero <bqromero@gmail.com>
2021-08-20 15:44:00 -0300

    Estados inseridos na pasta Centro Oeste

--- estados.bat
+++ estados.bat
@@ -1,4 +1,12 @@
 md "Todos os estados"
 cd "Todos os estados"
 
-md "Centro-Oeste" "Nordeste" "Norte" "Sul" "Sudeste"
+md "Centro Oeste" "Nordeste" "Norte" "Sul" "Sudeste"
+
+cd "Centro Oeste"
+md "GO" "MT" "MS" "DF"
+
+cd..
+
+cd Nordeste
+md "AL" "BA" "CE" "MA" "PB" "PI" "PE" "RN" "SE"
