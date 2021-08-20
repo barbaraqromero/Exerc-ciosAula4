bf3c2e4a51b7c5c519d3d015f8bad60e1a2d261e
Bárbara Romero <bqromero@gmail.com>
2021-08-20 16:08:17 -0300

    Pstas criada para cada pais

--- paises.bat
+++ paises.bat
@@ -1,1 +1,5 @@
 md "Todos os paises"
+cd "Todos os paises"
+
+md "Argentina" "Bolivia" "Brasil" "Chile" "Colombia" "Equador" "Guiana" "Paraguai" "Peru" "Suriname" "Uruguai" "Venezuela" "Guiana Francesa"
+
