59391cb0d2eec25f73f2565571cc98659f1d8223
Bárbara Romero <bqromero@gmail.com>
2021-08-20 15:16:43 -0300

    Times incluídos na pasta Serie A

--- clubes.bat
+++ clubes.bat
@@ -2,3 +2,8 @@ md "Todos os clubes"
 cd "Todos os clubes"
 
 md "Serie A" "Serie B"
+
+cd "Serie A"
+md "Atletico MG" "Palmeiras" "Fortaleza" "Bragantino" "Flamengo" "Atletico PR" "Atletico GO" "Ceara" "Internacional" "Santos" "Corinthians" "Juventude" "Bahia" "São Paulo" "Fluminense" "Cuiabá" "Sport" "America MG" "Gremio" "Chapecoense"
+
+
