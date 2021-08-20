ae80f4c4fba3faee285fe93c724dc21526664061
Bárbara Romero <bqromero@gmail.com>
2021-08-20 15:22:09 -0300

    Times incluídos na pasta Serie B

--- clubes.bat
+++ clubes.bat
@@ -6,4 +6,7 @@ md "Serie A" "Serie B"
 cd "Serie A"
 md "Atletico MG" "Palmeiras" "Fortaleza" "Bragantino" "Flamengo" "Atletico PR" "Atletico GO" "Ceara" "Internacional" "Santos" "Corinthians" "Juventude" "Bahia" "São Paulo" "Fluminense" "Cuiabá" "Sport" "America MG" "Gremio" "Chapecoense"
 
+cd..
 
+cd "Serie B"
+md "Coritiba" "Goias" "Avai" "CRB" "Sampaio Correa" "Guarani" "Nautico" "Botafogo" "Operario PR" "CSA" "Vasco" "Remo" "Brusque" "Cruzeiro" "Vila Nova" "Ponte Preta" "Londrina" "Vitória" "Confiança" "Brasil de Pelotas" 
