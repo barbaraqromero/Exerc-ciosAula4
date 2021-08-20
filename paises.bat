07c300822349e8bb8048391f108e713e3751a0a6
Bárbara Romero <bqromero@gmail.com>
2021-08-20 16:14:09 -0300

    Estados incluídos na pasta Brasil

--- paises.bat
+++ paises.bat
@@ -3,3 +3,11 @@ cd "Todos os paises"
 
 md "Argentina" "Bolivia" "Brasil" "Chile" "Colombia" "Equador" "Guiana" "Paraguai" "Peru" "Suriname" "Uruguai" "Venezuela" "Guiana Francesa"
 
+cd "Brasil"
+md "AC" "AL" "AP" "AM" "BA" "CE" "ES" "GO" "MA" "MT" "MS" "MG" "PA" "PB" "PR" "PE" "PI" "RJ" "RN" "RS" "RO" "RR" "SC" "SP" "SE" "TO" "DF"
+
+
+
+
+
+
